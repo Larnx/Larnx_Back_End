a9d0122fe7443a34c678deef89fe3eb0334acf98
christtina <christina.vhoward@gmail.com>
2018-04-25 22:04:25 -0400

    Recording for when camera is cropped and when camera is working correctly

--- recordVideo.cpp
+++ recordVideo.cpp
@@ -46,12 +46,16 @@ void readRectify(VideoCapture capLeft, VideoCapture capRight, int& num_images,
 	destroyAllWindows;
 }
 
-void splitImage(Mat frame, Mat& leftFrame, Mat& rightFrame) {
+void splitImage(Mat frame, Mat& leftFrameFull, Mat& rightFrameFull, Mat& leftFrameCropped, Mat& rightFrameCropped) {
 	int img_size = frame.size().width;
 	/*leftFrame = frame(cv::Range(0, frame.size().height), cv::Range(0, (img_size / 2) - 1));
 	rightFrame = frame(cv::Range(0, frame.size().height), cv::Range((img_size / 2) + 1, img_size));*/
-	leftFrame = frame(cv::Range(0, 2 * (frame.size().height / 3)), cv::Range(0, (img_size / 2) - 1));
-	rightFrame = frame(cv::Range((frame.size().height / 3), frame.size().height), cv::Range((img_size / 2) + 1, img_size));
+	// Incase the camera is working correctly
+	leftFrameFull = frame(cv::Range(0, frame.size().height), cv::Range(0, (img_size / 2) - 1));
+	rightFrameFull = frame(cv::Range(0, frame.size().height), cv::Range((img_size / 2) + 1, img_size));
+	// Incase the camera is not working
+	leftFrameCropped = frame(cv::Range(0, frame.size().height - 60), cv::Range(0, (img_size / 2) - 1));
+	rightFrameCropped = frame(cv::Range(60, frame.size().height), cv::Range((img_size / 2) + 1, img_size));
 }
 
 int main() {
@@ -64,7 +68,8 @@ int main() {
 
 
 	string left_vid, right_vid;
-	char* image_folder = "cb3";
+	//char* image_folder = "cb3";
+	cout << "You do not need to include .avi" << endl;
 	cout << "Left video filename: ";
 	cin >> left_vid;
 	cout << "\n";
@@ -72,34 +77,54 @@ int main() {
 	cin >> right_vid;
 	cout << "\n";
 
-	/*int frame_width1 = (vcap.get(CV_CAP_PROP_FRAME_WIDTH) / 2) - 1;
-	int frame_height1 = vcap.get(CV_CAP_PROP_FRAME_HEIGHT);
-	int frame_width2 = (vcap.get(CV_CAP_PROP_FRAME_WIDTH) / 2) - 1;
-	int frame_height2 = vcap.get(CV_CAP_PROP_FRAME_HEIGHT);*/
-	int frame_width1 = (vcap.get(CV_CAP_PROP_FRAME_WIDTH) / 2) - 1;
-	int frame_height1 = 2 * (vcap.get(CV_CAP_PROP_FRAME_HEIGHT) / 3);
-	int frame_width2 = (vcap.get(CV_CAP_PROP_FRAME_WIDTH) / 2) - 1;
-	int frame_height2 = 2 * (vcap.get(CV_CAP_PROP_FRAME_HEIGHT) / 3);
-	VideoWriter video1(left_vid, CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(frame_width1, frame_height1), true);
-	VideoWriter video2(right_vid, CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(frame_width2, frame_height2), true);
+	int frame_width1Full = (vcap.get(CV_CAP_PROP_FRAME_WIDTH) / 2) - 1;
+	int frame_height1Full = vcap.get(CV_CAP_PROP_FRAME_HEIGHT);
+	int frame_width2Full = (vcap.get(CV_CAP_PROP_FRAME_WIDTH) / 2) - 1;
+	int frame_height2Full = vcap.get(CV_CAP_PROP_FRAME_HEIGHT);
+
+	int frame_width1Cropped = (vcap.get(CV_CAP_PROP_FRAME_WIDTH) / 2) - 1;
+	int frame_height1Cropped = (vcap.get(CV_CAP_PROP_FRAME_HEIGHT) - 60);
+	int frame_width2Cropped = (vcap.get(CV_CAP_PROP_FRAME_WIDTH) / 2) - 1;
+	int frame_height2Cropped = (vcap.get(CV_CAP_PROP_FRAME_HEIGHT) - 60);
+
+	VideoWriter video1Full(left_vid + "left_full.avi", CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(frame_width1Full, frame_height1Full), true);
+	VideoWriter video2Full(right_vid + "right_full.avi", CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(frame_width2Full, frame_height2Full), true);
+
+	VideoWriter video1Cropped(left_vid + "left_cropped.avi", CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(frame_width1Cropped, frame_height1Cropped), true);
+	VideoWriter video2Cropped(right_vid + "right_cropped.avi", CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(frame_width2Cropped, frame_height2Cropped), true);
 
 
 	for (;;) {
 
-		Mat frame1, leftFrame, rightFrame;
+		Mat frame1, leftFrameF, rightFrameF, leftFrameC, rightFrameC;
 		vcap >> frame1;
-		imshow(frame1);
-		splitImage(frame1, leftFrame, rightFrame);
-		imshow("Frame1", leftFrame);
-		imshow("Frame2", rightFrame);
-		video1.write(leftFrame);
-		video2.write(rightFrame);
+		imshow("base frame", frame1);
+		splitImage(frame1, leftFrameF, rightFrameF, leftFrameC, rightFrameC);
+		imshow("FrameLF", leftFrameF);
+		imshow("FrameRF", rightFrameF);
+		imshow("FrameLC", leftFrameC);
+		imshow("FrameRC", rightFrameC);
+
+		video1Full.write(leftFrameF);
+		video2Full.write(rightFrameF);
+
+		video1Cropped.write(leftFrameC);
+		video2Cropped.write(rightFrameC);
 
 		char c = (char)waitKey(33);
 		if (c == 27) break;
 	}
+
+	/*left_vid = left_vid + ".avi";
+	right_vid = right_vid + ".avi";
 	VideoCapture cap1(left_vid);
 	VideoCapture cap2(right_vid);
+
+	left_vid = "Cropped" + left_vid;
+	right_vid = "Cropped" + right_vid;
+	VideoCapture cap3(left_vid);
+	VideoCapture cap4(right_vid);*/
+
 	//int num_imgs = 0;
 
 	//readRectify(cap1, cap2, num_imgs, cap1.get(CV_CAP_PROP_FRAME_WIDTH), cap1.get(CV_CAP_PROP_FRAME_HEIGHT), image_folder, image_folder, "jpg");
@@ -108,5 +133,5 @@ int main() {
 
 	return 0;
 
-	
+
 }
